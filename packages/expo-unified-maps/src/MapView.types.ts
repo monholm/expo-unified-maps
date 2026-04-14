38804f1b4952bb263e0d2baaffed7f16d834e91c
monholm <73996878+monholm@users.noreply.github.com>
2026-04-14 15:07:46 +0200

    feat(ts): disallow children

--- packages/expo-unified-maps/src/MapView.types.ts
+++ packages/expo-unified-maps/src/MapView.types.ts
@@ -3,6 +3,7 @@ import type {SharedRefType} from 'expo';
 
 export interface MapViewNativeProps extends ViewProps {
   boundary?: Region | undefined;
+  children?: never;
   /**
    * The initial region to display on the map.
    *
